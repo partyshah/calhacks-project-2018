1d7c1e8a117c2571e702350609a970ff6f77c75a
Elise Xu <elisexu98@gmail.com>
2018-11-03 15:34:06 -0700

    Forgot to take out extra function in App.js.

--- calhacks/src/App.js
+++ calhacks/src/App.js
@@ -6,22 +6,6 @@ import CityArea from './Components/CityArea';
 
 class App extends Component {
 
-  createCity() {
-    let buildings = [];
-    let startX = 0;
-    let endX = 400;
-    for (let i = 0; i < 20; i++) {
-        let buildingWidth = Math.floor((Math.random() * 10) + 1).toString() + "vw";
-        let buildingHeight = Math.floor((Math.random() * 10) + 1).toString() + "vh";
-        let buildingColor = "#000080";
-        console.log(buildingWidth);
-        console.log(buildingHeight)
-        console.log("fuk");
-        buildings.push(<TallBuilding key = {i} width={buildingWidth} height={buildingHeight} color = {buildingColor}/>)
-    }
-    return buildings;
-  }
-
   render() {
     return (
       <div className="App">
