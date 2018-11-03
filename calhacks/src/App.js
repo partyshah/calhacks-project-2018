1866bc286e8fe4fabc2ff6e83e226f4c1e101125
Elise Xu <elisexu98@gmail.com>
2018-11-03 15:22:22 -0700

    Created basic randomized skyline.

--- calhacks/src/App.js
+++ calhacks/src/App.js
@@ -1,25 +1,31 @@
 import React, { Component } from 'react';
+import TallBuilding from './Components/TallBuilding';
 import logo from './logo.svg';
 import './App.css';
+import CityArea from './Components/CityArea';
 
 class App extends Component {
+
+  createCity() {
+    let buildings = [];
+    let startX = 0;
+    let endX = 400;
+    for (let i = 0; i < 20; i++) {
+        let buildingWidth = Math.floor((Math.random() * 10) + 1).toString() + "vw";
+        let buildingHeight = Math.floor((Math.random() * 10) + 1).toString() + "vh";
+        let buildingColor = "#000080";
+        console.log(buildingWidth);
+        console.log(buildingHeight)
+        console.log("fuk");
+        buildings.push(<TallBuilding key = {i} width={buildingWidth} height={buildingHeight} color = {buildingColor}/>)
+    }
+    return buildings;
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <CityArea/>
       </div>
     );
   }
